fb5797ae233d87491abcccbbcade44203688f7a0
Kamyar Sehati <kamyarsehati@icloud.com>
2022-04-02 16:37:03 +0430

    Create WeatherViewController

---
+++ WeatherApp/WeatherApp/WeatherViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Kamyar on 4/2/22.
+//
+
+import UIKit
+
+final class WeatherViewController: WAScrollViewController {
+
+    private(set) lazy var backgroundView: WAGradientView = {
+        let view = WAGradientView()
+        view.configGradient(with: [.systemGray6, .systemGray5], .vertical)
+        return view
+    }()
+
+    override func loadView() {
+        super.loadView()
+
+        self.view.insertSubview(self.backgroundView, at: 0)
+        self.backgroundView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
