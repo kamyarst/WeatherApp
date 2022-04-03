b97f369efed623b80749f653e79942ee7dce180d
Kamyar Sehati <kamyarsehati@icloud.com>
2022-04-03 22:13:22 +0430

    Create UICollectionViewCell extension

---
+++ WeatherApp/WeatherApp/Extensions/UICollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Kamyar on 4/3/22.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var id: String {
+        String(describing: self)
+    }
+}
