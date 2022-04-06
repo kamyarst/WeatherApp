76db974e0efaca255a4013169a20a388bf9641c1
Kamyar Sehati <kamyarsehati@icloud.com>
2022-04-06 21:00:48 +0430

    Make properties of weatherModel public

--- WeatherCore/Weather/Weather Feature/WeatherModel.swift
+++ WeatherCore/Weather/Weather Feature/WeatherModel.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 public struct WeatherModel: Codable, Equatable {
+
     public let location: LocationModel
     public let current: CurrentModel
     public let forecast: ForecastModel
+
+    public init(location: LocationModel, current: CurrentModel, forecast: ForecastModel) {
+        self.location = location
+        self.current = current
+        self.forecast = forecast
+    }
 
     public static func == (lhs: WeatherModel, rhs: WeatherModel) -> Bool {
         lhs.location.id == rhs.location.id &&
@@ -22,12 +29,25 @@ public struct WeatherModel: Codable, Equatable {
 // MARK: - Current
 
 public struct CurrentModel: Codable {
-    let lastUpdatedEpoch: Int
-    let lastUpdated: String
-    let tempC, tempF: Double
-    let isDay: Int
-    let condition: ConditionModel
-    let feelslikeC, feelslikeF: Double
+
+    public let lastUpdatedEpoch: Int
+    public let lastUpdated: String
+    public let tempC, tempF: Double
+    public let isDay: Int
+    public let condition: ConditionModel
+    public let feelslikeC, feelslikeF: Double
+
+    public init(lastUpdatedEpoch: Int, lastUpdated: String, tempC: Double, tempF: Double, isDay: Int,
+                condition: ConditionModel, feelslikeC: Double, feelslikeF: Double) {
+        self.lastUpdatedEpoch = lastUpdatedEpoch
+        self.lastUpdated = lastUpdated
+        self.tempC = tempC
+        self.tempF = tempF
+        self.isDay = isDay
+        self.condition = condition
+        self.feelslikeC = feelslikeC
+        self.feelslikeF = feelslikeF
+    }
 
     enum CodingKeys: String, CodingKey {
         case lastUpdatedEpoch = "last_updated_epoch"
@@ -44,24 +64,45 @@ public struct CurrentModel: Codable {
 // MARK: - ConditionModel
 
 public struct ConditionModel: Codable {
-    let text, icon: String
-    let code: Int
+
+    public let text, icon: String
+    public let code: Int
+
+    public init(text: String, icon: String, code: Int) {
+        self.text = text
+        self.icon = icon
+        self.code = code
+    }
 }
 
 // MARK: - ForecastModel
 
 public struct ForecastModel: Codable {
-    let forecastday: [ForecastDayModel]
+
+    public let forecastday: [ForecastDayModel]
+
+    public init(forecastday: [ForecastDayModel]) {
+        self.forecastday = forecastday
+    }
 }
 
 // MARK: - ForecastDayModel
 
 public struct ForecastDayModel: Codable {
-    let date: String
-    let dateEpoch: Int
-    let day: DayModel
-    let astro: AstroModel
-    let hour: [HourModel]
+
+    public let date: String
+    public let dateEpoch: Int
+    public let day: DayModel
+    public let astro: AstroModel
+    public let hour: [HourModel]
+
+    public init(date: String, dateEpoch: Int, day: DayModel, astro: AstroModel, hour: [HourModel]) {
+        self.date = date
+        self.dateEpoch = dateEpoch
+        self.day = day
+        self.astro = astro
+        self.hour = hour
+    }
 
     enum CodingKeys: String, CodingKey {
         case date
@@ -73,7 +114,13 @@ public struct ForecastDayModel: Codable {
 // MARK: - AstroModel
 
 public struct AstroModel: Codable {
-    let sunrise, sunset: String
+
+    public let sunrise, sunset: String
+
+    public init(sunrise: String, sunset: String) {
+        self.sunrise = sunrise
+        self.sunset = sunset
+    }
 
     enum CodingKeys: String, CodingKey {
         case sunrise
@@ -84,13 +131,30 @@ public struct AstroModel: Codable {
 // MARK: - DayModel
 
 public struct DayModel: Codable {
-    let maxtempC, maxtempF: Double
-    let mintempC, mintempF: Double
-    let avgtempC, avgtempF: Double
 
-    let dailyWillItRain, dailyChanceOfRain: Int
-    let dailyWillItSnow, dailyChanceOfSnow: Int
-    let condition: ConditionModel
+    public let maxtempC, maxtempF: Double
+    public let mintempC, mintempF: Double
+    public let avgtempC, avgtempF: Double
+
+    public let dailyWillItRain, dailyChanceOfRain: Int
+    public let dailyWillItSnow, dailyChanceOfSnow: Int
+    public let condition: ConditionModel
+
+    public init(maxtempC: Double, maxtempF: Double, mintempC: Double, mintempF: Double, avgtempC: Double,
+                avgtempF: Double, dailyWillItRain: Int, dailyChanceOfRain: Int, dailyWillItSnow: Int,
+                dailyChanceOfSnow: Int, condition: ConditionModel) {
+        self.maxtempC = maxtempC
+        self.maxtempF = maxtempF
+        self.mintempC = mintempC
+        self.mintempF = mintempF
+        self.avgtempC = avgtempC
+        self.avgtempF = avgtempF
+        self.dailyWillItRain = dailyWillItRain
+        self.dailyChanceOfRain = dailyChanceOfRain
+        self.dailyWillItSnow = dailyWillItSnow
+        self.dailyChanceOfSnow = dailyChanceOfSnow
+        self.condition = condition
+    }
 
     enum CodingKeys: String, CodingKey {
         case maxtempC = "maxtemp_c"
@@ -110,14 +174,32 @@ public struct DayModel: Codable {
 // MARK: - HourModel
 
 public struct HourModel: Codable {
-    let timeEpoch: Int
-    let time: String
-    let tempC, tempF: Double
-    let isDay: Int
-    let condition: ConditionModel
-    let feelslikeC, feelslikeF: Double
-    let willItRain, chanceOfRain: Int
-    let willItSnow, chanceOfSnow: Int
+
+    public let timeEpoch: Int
+    public let time: String
+    public let tempC, tempF: Double
+    public let isDay: Int
+    public let condition: ConditionModel
+    public let feelslikeC, feelslikeF: Double
+    public let willItRain, chanceOfRain: Int
+    public let willItSnow, chanceOfSnow: Int
+
+    public init(timeEpoch: Int, time: String, tempC: Double, tempF: Double, isDay: Int,
+                condition: ConditionModel, feelslikeC: Double, feelslikeF: Double, willItRain: Int,
+                chanceOfRain: Int, willItSnow: Int, chanceOfSnow: Int) {
+        self.timeEpoch = timeEpoch
+        self.time = time
+        self.tempC = tempC
+        self.tempF = tempF
+        self.isDay = isDay
+        self.condition = condition
+        self.feelslikeC = feelslikeC
+        self.feelslikeF = feelslikeF
+        self.willItRain = willItRain
+        self.chanceOfRain = chanceOfRain
+        self.willItSnow = willItSnow
+        self.chanceOfSnow = chanceOfSnow
+    }
 
     enum CodingKeys: String, CodingKey {
         case timeEpoch = "time_epoch"
